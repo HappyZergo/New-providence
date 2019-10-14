e2c74a6a1ca5fdbcfe362da08f0df54c9c3bc1d7
kravchenko <dimak622@gmail.com>
2019-10-14 13:21:04 +0300

    добавил анимации

--- js/main.js
+++ js/main.js
@@ -1,26 +1,35 @@
 $(document).ready(function() {
 
-            var individual_price_table = $('#price_tables').find('.individual');
-            var company_price_table = $('#price_tables').find('.company');
+    wow = new WOW({
+        boxClass: 'wow', // default
+        animateClass: 'animated', // default
+        offset: 0, // default
+        mobile: true, // default
+        live: true // default
+    })
+    wow.init();
+
+    var individual_price_table = $('#price_tables').find('.individual');
+    var company_price_table = $('#price_tables').find('.company');
 
 
-            $('.switch-toggles').find('.individual').addClass('active');
-            $('#price_tables').find('.individual').addClass('active');
+    $('.switch-toggles').find('.individual').addClass('active');
+    $('#price_tables').find('.individual').addClass('active');
 
-            $('.switch-toggles').find('.individual').on('click', function() {
-                $(this).addClass('active');
-                $(this).closest('.switch-toggles').removeClass('active');
-                $(this).siblings().removeClass('active');
-                individual_price_table.addClass('active');
-                company_price_table.removeClass('active');
-            });
+    $('.switch-toggles').find('.individual').on('click', function() {
+        $(this).addClass('active');
+        $(this).closest('.switch-toggles').removeClass('active');
+        $(this).siblings().removeClass('active');
+        individual_price_table.addClass('active');
+        company_price_table.removeClass('active');
+    });
 
-            $('.switch-toggles').find('.company').on('click', function() {
-                $(this).addClass('active');
-                $(this).closest('.switch-toggles').addClass('active');
-                $(this).siblings().removeClass('active');
-                company_price_table.addClass('active');
-                individual_price_table.removeClass('active');
-            });
-            
+    $('.switch-toggles').find('.company').on('click', function() {
+        $(this).addClass('active');
+        $(this).closest('.switch-toggles').addClass('active');
+        $(this).siblings().removeClass('active');
+        company_price_table.addClass('active');
+        individual_price_table.removeClass('active');
+    });
+
 });
